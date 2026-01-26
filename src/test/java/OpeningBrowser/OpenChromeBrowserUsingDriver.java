8b8fdda028c3c913790d67011a8de852b8306fde
Sayed Mohamed <syd85436@gmail.com>
2026-01-26 15:40:36 +0200

    Creating some functions

--- src/test/java/OpeningBrowser/OpenChromeBrowserUsingDriver.java
+++ src/test/java/OpeningBrowser/OpenChromeBrowserUsingDriver.java
@@ -1,12 +1,13 @@
-package basics;
+package OpeningBrowser;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenChromeBrowserUsingDriver {
 
     public static void main(String[] args) {
-       System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
     }
 }
---
+++ src/test/java/VisitingWebsites/OpenHTMLFile.java
@@ -0,0 +1,16 @@
+package VisitingWebsites;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenHTMLFile {
+
+
+    public static void main(String[] args){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("file://C:\\Users\\pc\\IdeaProjects\\QAcart_Selenium\\src\\test\\resources\\index.html");
+    }
+}
